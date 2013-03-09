3ce3b1cb59644198ead797f0ff1580673921ebc5
Terry Simons <terry.simons@gmail.com>
2013-03-09 00:49:56 -0800

    Colors in PyGame are 255 based, not 1.0 based.

--- pyguts/spine/Slot.py
+++ pyguts/spine/Slot.py
@@ -1,13 +1,19 @@
 class Slot(object):
-    def __init__(self, data, skeleton, bone):
+    def __init__(self, slotData, skeleton, bone):
         super(Slot, self).__init__()
-        self.data = data
+        if not slotData:
+            raise Exception('slotData cannot be None.')
+        if not skeleton:
+            raise Exception('skeleton cannot be None.')
+        if not bone:
+            raise Exception('bone cannot be None.')
+        self.data = slotData
         self.skeleton = skeleton
         self.bone = bone
-        self.r = 1.0
-        self.g = 1.0
-        self.b = 1.0
-        self.a = 1.0
+        self.r = 255
+        self.g = 255
+        self.b = 255
+        self.a = 255
         self.attachment = None
         self.attachmentTime = 0.0
 
@@ -20,6 +26,13 @@ class Slot(object):
 
         #C++ does this, but Corona doesn't.
         #self.setToBindPose()
+
+
+    def setColor(self, r, g, b, a):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
                           
         
     def setAttachment(self, attachment):
@@ -42,10 +55,7 @@ class Slot(object):
 
 
     def setToBindPoseWithIndex(self, slotIndex):
-        self.r = self.data.r
-        self.g = self.data.g
-        self.b = self.data.b
-        self.a = self.data.a
+        self.setColor(self.data.r, self.data.g, self.data.b, self.data.a)
         self.setAttachment(self.skeleton.getAttachmentByIndex(slotIndex, self.data.attachmentName) if self.data.attachmentName else None)
     
     
--- pyguts/spine/SlotData.py
+++ pyguts/spine/SlotData.py
@@ -1,10 +1,24 @@
 class SlotData(object):
     def __init__(self, name, boneData):
         super(SlotData, self).__init__()
+        if not name:
+            raise Exception('Name cannot be None.')
+
+        if not boneData:
+            raise Exception('boneData cannot be None.')
+
         self.name = name
         self.boneData = boneData
-        self.r = 1.0
-        self.g = 1.0
-        self.b = 1.0
-        self.a = 1.0
+        self.r = 255
+        self.g = 255
+        self.b = 255
+        self.a = 255
         self.attachmentName = None
+
+
+    def setColor(r, g, b, a):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+
