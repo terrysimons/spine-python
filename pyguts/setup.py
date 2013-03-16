52e3ee7f3ce8868784c850278955d4eb9b9e1413
Terry Simons <terry.simons@gmail.com>
2013-03-16 05:08:11 -0700

    Fix setup.py scripts.

--- pyguts/setup.py
+++ pyguts/setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-                                                                                                                                                                            
-from distutils.core import setup
+
+from setuptools import setup
 
 setup(name='pyguts',
       version='1.0b1',
       description='A pygame front-end for spine-python.',
       author='Terry Simons',
       author_email='terry.simons@gmail.com',
-      url='https://github.com/terrysimons/spine-python',
+      url='https://github.com/terrysimons/spine-python/pyguts',
       package_dir={'pyguts': 'src'},
-      packages=['pyguts']
+      packages=['pyguts'],
+      install_requires=['spine_python']
      )
--- spine-python/setup.py
+++ spine-python/setup.py
@@ -7,7 +7,7 @@ setup(name='spine-python',
       description='A Pure Python Spine runtime.',
       author='Terry Simons',
       author_email='terry.simons@gmail.com',
-      url='https://github.com/terrysimons/spine-python',
+      url='https://github.com/terrysimons/spine-python/spine-python',
       package_dir={'spine': 'src'},
       packages=['spine', 'spine.Atlas', 'spine.Animation'],
      )
