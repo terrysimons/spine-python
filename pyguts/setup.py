cb6ff8784d1fa69ffd937dbce75201e785f28f71
Terry Simons <terry.simons@gmail.com>
2013-03-16 04:20:11 -0700

    Turn pyguts into a distutils module.

---
+++ pyguts/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+                                                                                                                                                                            
+from distutils.core import setup
+
+setup(name='pyguts',
+      version='1.0',
+      description='A pygame front-end for spine-python.',
+      author='Terry Simons',
+      author_email='terry.simons@gmail.com',
+      url='https://github.com/terrysimons/spine-python',
+      package_dir={'pyguts': 'src'},
+      packages=['pyguts']
+     )
