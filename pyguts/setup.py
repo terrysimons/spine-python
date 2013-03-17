286b0a8f26a2716691f2fa26b1b39bab700bd4e4
Terry Simons <terry.simons@gmail.com>
2013-03-16 17:13:58 -0700

    Add manifest file.

--- pyguts/setup.py
+++ pyguts/setup.py
@@ -3,14 +3,13 @@
 from setuptools import setup
 
 setup(name='pyguts',
-      version='1.0b2',
+      version='1.0b4',
       description='A pygame front-end for spine-python.',
       author='Terry Simons',
       author_email='terry.simons@gmail.com',
       url='https://github.com/terrysimons/spine-python/pyguts',
       package_dir={'pyguts': 'src'},
       packages=['pyguts'],
-      package_data={'examples': ['examples/*']},
       classifiers=['License :: OSI Approved :: BSD License'],
       install_requires=['spine_python']
      )
